52a1fa458dee4dfb1a36ef2b0732956163f63e2d
user2292637002 <2292637002@qq.com>
2025-09-15 17:51:18 +0800

    贪心算法 add

---
+++ MyAlgorithm/MergeIntervals/main.cpp
@@ -0,0 +1,65 @@
+﻿/*
+题目描述：
+给出一个区间的集合，合并所有重叠的区间。
+
+示例：
+输入: intervals = [[1,3],[2,6],[8,10],[15,18]]
+输出: [[1,6],[8,10],[15,18]]
+解释: 区间 [1,3] 和 [2,6] 重叠，合并为 [1,6]。
+
+提示：
+先按区间的起始位置排序。
+遍历区间，如果当前区间的起点 ≤ 上一个区间的终点，则合并（更新终点为最大值）。
+否则，将当前区间加入结果。
+
+*/
+
+#include<iostream>
+#include<algorithm>
+#include<vector>
+using namespace std;
+
+vector<vector<int>> MergeIntervals(vector<vector<int>> intervals)
+{
+	if (intervals.empty()) return {};
+
+	sort(intervals.begin(), intervals.end());
+	
+	vector<vector<int>> merged;
+	merged.push_back(intervals[0]);
+	for (int i = 0; i < intervals.size(); i++)
+	{
+		if (intervals[i][0] <= merged.back()[1])
+		{
+			merged.back()[1] = max(merged.back()[1], intervals[i][1]);
+		}
+		else
+		{
+			merged.push_back(intervals[i]);
+		}
+	}
+	return merged;
+}
+
+int main()
+{
+	vector<vector<int>> intervals = { {1,3},{2,6},{8,10},{15,18} };
+
+	auto merged = MergeIntervals(intervals);
+
+	cout << "[";
+	for (int i = 0; i < merged.size(); i++)
+	{
+		cout << "[";
+		for (int j = 0; j < merged[i].size(); j++)
+		{
+			cout << merged[i][j];
+			if (j != merged[i].size() - 1)
+				cout << ",";
+		}
+		cout << "]";
+		if (i != merged.size()-1)
+		cout << ",";
+	}
+	cout << "]";
+}
---
+++ MyAlgorithm/SearchinRotatedSortedArray/main.cpp
@@ -0,0 +1,57 @@
+﻿/*
+题目描述：
+整数数组 nums 按升序排列，数组在某个未知下标 k 处进行了旋转（例如 [0,1,2,4,5,6,7] 变成 [4,5,6,7,0,1,2]）。
+给定目标值 target，如果数组中存在这个目标值，则返回它的下标，否则返回 -1。
+
+示例：
+输入: nums = [4,5,6,7,0,1,2], target = 0
+输出: 4
+
+提示：
+旋转数组可以分为两段有序部分。
+判断 mid 在哪一段，再判断 target 是否在有序的那一段中。
+
+*/
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+int search(vector<int>& nums, int target) {
+    int left = 0, right = nums.size() - 1;
+
+    while (left <= right) {
+        int mid = left + (right - left) / 2;
+        if (nums[mid] == target) return mid;
+
+        // 判断左半段是否有序
+        if (nums[left] <= nums[mid]) {
+            if (target >= nums[left] && target < nums[mid]) {
+                right = mid - 1;
+            }
+            else {
+                left = mid + 1;
+            }
+        }
+        // 右半段有序
+        else {
+            if (target > nums[mid] && target <= nums[right]) {
+                left = mid + 1;
+            }
+            else {
+                right = mid - 1;
+            }
+        }
+    }
+    return -1;
+}
+
+
+int main()
+{
+    vector<int> nums = { 4,5,6,7,0,1,2 };
+    int target = 0;
+    cout << search(nums, target);
+	return 0;
+}
